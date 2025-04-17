9ffaa921e982bb5176ab6bfa9b7205862cd2c2ca
hlebosos <19miklesurg@gmail.com>
2025-04-17 20:57:16 +0300

    Code 3.7
    New code for old Python(3.7)

--- wordle_Python3-7.py
+++ wordle_Python3-7.py
@@ -6,7 +6,6 @@ import time
 from colorama import init
 init()
 
-
 alphabet = ["а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о",
             "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я"]
 
@@ -18,16 +17,13 @@ def clear_screen():
 
 def move_cursor(x, y):
     # ANSI escape последовательность для перемещения курсора
-    sys.stdout.write(f"\033[{y};{x}H")
+    sys.stdout.write("\033[{};{}H".format(y, x))
 
 
 def russianWord(text):
     for letter in text:
         if letter.lower() not in alphabet:
             break
-        else:
-            continue
-        break
     else:
         return True
     return False
@@ -36,19 +32,17 @@ def russianWord(text):
 script_dir = os.path.dirname(__file__)
 word5_file_path = os.path.join(script_dir, 'word5.txt')
 
-
 num_word = -1
 hidden_word = []
 word = ""
 
 
 def randomWord(mode):
-
     with open(word5_file_path, "r", encoding="utf-8") as file:
         lines = file.readlines()
-    file.close()
     if mode == 0:
-        num_word = random.randint(1, len(lines))
+        # Изменено на -1 для индексации
+        num_word = random.randint(1, len(lines)) - 1
     elif mode == 1:
         num_word = -1
     return list(lines[num_word].replace("\n", "").upper()), num_word
@@ -62,45 +56,43 @@ def addWord():
         if russianWord(word_to_add) and len(word_to_add) == 5:
             word_is_true = True
         else:
-            print(Style.DIM+Fore.RED, end="")
+            print(Style.DIM + Fore.RED, end="")
             for _ in "Введите слово из 5 букв русского алфавита!\n":
                 print(_, end="", flush=True)
                 time.sleep(0.03)
-                print(Style.RESET_ALL, end="")
+            print(Style.RESET_ALL, end="")
 
-    else:
-        with open(word5_file_path, "r", encoding="utf-8") as file:
-            lines = file.readlines()
+    with open(word5_file_path, "r", encoding="utf-8") as file:
+        lines = file.readlines()
 
-        if lines:
-            lines[-1] = word_to_add.lower()
+    if lines:
+        lines[-1] = word_to_add.lower() + "\n"  # Добавлен перенос строки
 
-        with open(word5_file_path, "w", encoding="utf-8") as file:
-            file.writelines(lines)
+    with open(word5_file_path, "w", encoding="utf-8") as file:
+        file.writelines(lines)
 
 
 def chooseLevevl():
     print("\nВыбор режима:")
-    print(Style.BRIGHT+Fore.LIGHTMAGENTA_EX)
+    print(Style.BRIGHT + Fore.LIGHTMAGENTA_EX)
     for _ in " обычный режим -> 0":
         print(_, end="", flush=True)
         time.sleep(0.04)
-    print(Style.BRIGHT+Fore.LIGHTCYAN_EX)
+    print(Style.BRIGHT + Fore.LIGHTCYAN_EX)
     for _ in " режим загадывания слова -> 1":
         print(_, end="", flush=True)
         time.sleep(0.04)
-    print(Style.BRIGHT+Fore.LIGHTRED_EX)
+    print(Style.BRIGHT + Fore.LIGHTRED_EX)
     for _ in " выйти -> Ctrl+C\n":
         print(_, end="", flush=True)
         time.sleep(0.04)
     print(Style.RESET_ALL)
 
-    if (m := input("Режим: ")) == "0":
+    m = input("Режим: ")
+    if m == "0":
         Game(0)
     elif m == "1":
-
         addWord()
-
         Game(1)
     else:
         print(Style.BRIGHT+Back.WHITE+Fore.RED +
@@ -114,10 +106,9 @@ def chooseLevevl():
         time.sleep(3)
         Game(m)
 
-    # Enter Game
-
 
 def Game(mode):
+    global hidden_word, num_word  # Добавлено для использования глобальных переменных
     if mode == 0:
         hidden_word, num_word = randomWord(0)
     elif mode == 1:
@@ -129,7 +120,7 @@ def Game(mode):
     clear_screen()
     move_cursor(0, 0)
 
-    print(Style.BRIGHT+Fore.CYAN+"Слово загадано, У тебя 6 попыток")
+    print(Style.BRIGHT + Fore.CYAN + "Слово загадано, У тебя 6 попыток")
     print("Введите слова: " + Style.RESET_ALL)
     used_leters = {_.upper(): 0 for _ in alphabet}
     posX, posY = 0, 3
@@ -147,30 +138,30 @@ def Game(mode):
                 move_cursor(posX, posY)
 
                 for _ in "Введите слово из 5 букв русского алфавита!":
-                    print(Style.DIM+Fore.RED+_, end="", flush=True)
+                    print(Style.DIM + Fore.RED + _, end="", flush=True)
                     time.sleep(0.03)
                 time.sleep(0.5)
                 move_cursor(posX, posY)
-                print(Style.RESET_ALL+" "*50)
+                print(Style.RESET_ALL + " " * 50)
                 move_cursor(posX, posY)
 
         word = list(word.replace("\n", ""))
+
         move_cursor(posX, posY)
 
         if word == hidden_word:
             for h in word:
-                print(Style.BRIGHT + Back.GREEN +
-                      h + Style.RESET_ALL, end=" ", flush=True)
+                print(Style.BRIGHT + Back.GREEN + h +
+                      Style.RESET_ALL, end=" ", flush=True)
                 time.sleep(0.2)
 
-            for _ in f"Ты угадал! Молодец!\n{("('・ω・')", "(/◕ヮ◕)/", "ᶘᵒᴥᵒᶅ", "( ͡° ͜ʖ ͡°)")[random.randint(0, 3)]}\n":
-                print(Fore.MAGENTA+_+Style.RESET_ALL, end="", flush=True)
+            for _ in ["Ты угадал! Молодец!\n", "Ты молодец!\n", "Поздравляю!\n"][random.randint(0, 2)]:
+                print(Fore.MAGENTA + _ + Style.RESET_ALL, end="", flush=True)
                 time.sleep(0.08)
 
             break
 
         for l in range(len(word)):
-
             if word[l] in hidden_word:
                 if word[l] == hidden_word[l]:
                     print(Style.BRIGHT + Back.GREEN +
@@ -190,17 +181,14 @@ def Game(mode):
 
             time.sleep(0.1)
 
-        # print(used_leters)
-
         p1, p2 = 0, 0
 
         for k, v in used_leters.items():
-
-            move_cursor(60+p1, 5+p2)
+            move_cursor(60 + p1, 5 + p2)
             print((Back.BLACK + Style.RESET_ALL, Style.BRIGHT + Back.GREEN, Style.BRIGHT + Back.YELLOW,
-                  Back.BLACK + Fore.LIGHTRED_EX)[v]+str(k) + Style.RESET_ALL, end="")
+                   Back.BLACK + Fore.LIGHTRED_EX)[v] + str(k) + Style.RESET_ALL, end="")
             p1 += 3
-            if (p1+15) % 20 == 0:
+            if (p1 + 15) % 20 == 0:
                 p1 = 0
                 p2 += 1
         else:
@@ -210,47 +198,45 @@ def Game(mode):
         posY += 2
         move_cursor(0, posY)
     else:
-        for _ in f"К сожилению ты проиграл... {("ಠ_ಠ", "ಠxಠ", "(⚆_⚆)", "☹", "(-ㅅ-)", "(・へ・)", "(-﹏-；)")[random.randint(0, 6)]}\nПовезёт в следующий раз!":
-            print(Fore.MAGENTA+_+Style.RESET_ALL, end="", flush=True)
+        for _ in ["К сожалению ты проиграл... ", "Попробуй снова! ", "Не повезло! "][random.randint(0, 2)]:
+            print(Fore.MAGENTA + _ + Style.RESET_ALL, end="", flush=True)
             time.sleep(0.05)
         print("")
-        print(Fore.YELLOW+Back.BLACK +
-              f"Загаданое слово - {"".join(hidden_word)}"+Fore.BLACK+f"index={num_word+1}" + Style.RESET_ALL)
+        print(Fore.YELLOW + Back.BLACK + "Загаданое слово - " + "".join(hidden_word) +
+              Fore.BLACK + " index=" + str(num_word + 1) + Style.RESET_ALL)
 
     contin(mode)
 
 
 def main():
-
     # Wordle
-    print("-----------------"+Fore.BLACK+Back.YELLOW+"W" + Style.RESET_ALL + " "
-          + Fore.YELLOW+Back.BLACK+"O" + Style.RESET_ALL + " "
-          + Fore.BLACK+Back.YELLOW+"R" + Style.RESET_ALL + " "
-          + Fore.YELLOW+Back.BLACK+"D" + Style.RESET_ALL + " "
-          + Fore.BLACK+Back.YELLOW+"L" + Style.RESET_ALL + " "
-          + Fore.YELLOW+Back.BLACK+"E" + Style.RESET_ALL + " "
-          + Style.RESET_ALL+"----------------")
+    print("-----------------" + Fore.BLACK + Back.YELLOW + "W" + Style.RESET_ALL + " "
+          + Fore.YELLOW + Back.BLACK + "O" + Style.RESET_ALL + " "
+          + Fore.BLACK + Back.YELLOW + "R" + Style.RESET_ALL + " "
+          + Fore.YELLOW + Back.BLACK + "D" + Style.RESET_ALL + " "
+          + Fore.BLACK + Back.YELLOW + "L" + Style.RESET_ALL + " "
+          + Fore.YELLOW + Back.BLACK + "E" + Style.RESET_ALL + " "
+          + Style.RESET_ALL + "----------------")
 
     time.sleep(0.2)
 
     # logo
     print("                                   ", end="")
-
     for _ in "by HLeeb_":
-        print(Fore.CYAN+_, end="", flush=True)
+        print(Fore.CYAN + _, end="", flush=True)
         time.sleep(0.1)
     else:
-        print("\n"+Style.RESET_ALL)
+        print("\n" + Style.RESET_ALL)
     time.sleep(0.2)
 
     # rule
     print("Правила очень просты:",
-          "•Загадано слово из 5 русских букв.",
-          "•Каждым ходом игрок вводит свое слово, а програма определяет какие буквы входят в слово.",
-          " " + Style.BRIGHT+Back.GREEN + " " + Style.RESET_ALL +
+          "• Загадано слово из 5 русских букв.",
+          "• Каждым ходом игрок вводит свое слово, а программа определяет, какие буквы входят в слово.",
+          " " + Style.BRIGHT + Back.GREEN + " " + Style.RESET_ALL +
           " " + "- Буква есть в слове и стоит на своём месте",
-          " " + Style.BRIGHT+Back.YELLOW + " " + Style.RESET_ALL +
-          " " + "- Буква есть в слове, но она стоит не на своём месте",
+          " " + Style.BRIGHT + Back.YELLOW + " " + Style.RESET_ALL +
+          " " + "- Буква есть в слов е, но она стоит не на своём месте",
           " " + Back.LIGHTBLACK_EX + " " + Style.RESET_ALL +
           " " + "- Буквы нет в загаданом слове", "Удачи!",
           sep="\n")
